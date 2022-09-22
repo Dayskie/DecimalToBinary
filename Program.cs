91b47f52f220a531698566672f3dff01dbaa180b
Brody <mondomontage@gmail.com>
2022-09-22 20:05:43 +1000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int input);
+            string binary = "";
+            
+            while (input != 0)
+            {
+                binary += (input % 2).ToString();
+                input /= 2;
+            }
+            Console.Write(Flipped(binary));
+        }
+
+        public static string Flipped(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
